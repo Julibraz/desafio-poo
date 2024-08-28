6ef11f48af9b96b45fd0210da7a522d3f328314f
Julimar jr <jotajulimar.junior@gmail.com>
2024-08-27 21:54:50 -0300

    algumas alterações

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,23 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
+        Curso curso1 = new Curso();
+
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Descrição do curso");
+        curso1.setCargaHoraria(8);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria de Java");
+        mentoria.setDescricao("Descrição da mentoria");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(mentoria);
 
     }
 }
